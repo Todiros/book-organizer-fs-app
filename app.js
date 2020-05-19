4621251527bb8fb773f30dc0b624c1812360ddcc
Tony Borisov <tony.d.borisov@gmail.com>
2020-05-19 05:09:27 +0300

    feat: adds routers and html files

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const hostname = '127.0.0.1'
 const port = 5000
 
 const app = express()
 
-app.use('/products', (req, res, next) => {
-    res.send('<h1>Products Y\'all!</h1>')
-})
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Sup\' Express!</h1>')
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res) => {
+    res.status(404).sendFile(
+        path.join(__dirname, 'views', '404.html')
+    )
 })
 
 app.listen(port, hostname, () => {
---
+++ routes/admin.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+const router = express.Router()
+
+router.get('/add-product', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+})
+
+router.post('/add-product', (req, res) => {
+    res.redirect('/')
+})
+
+module.exports = router
